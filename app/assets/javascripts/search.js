6e6eeee96e67b58fb55ebcd5d9eb3fc66c4f7b93
Shinichiro Miyazono <shinichiro.miyazono@gmail.com>
2019-08-24 13:00:59 +0900

    Create search.js

---
+++ app/assets/javascripts/search.js
@@ -0,0 +1,14 @@
+$(function() {
+  $(function() {
+  $(".chat-group-form__input").on("keyup", function() {
+    var input = $("#user-search-field").val();
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json'
+    })
+    
+  });
+  });
+});
