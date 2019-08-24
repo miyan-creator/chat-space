b40690b83c1d643fec4338d1de58c114b19a060e
Shinichiro Miyazono <shinichiro.miyazono@gmail.com>
2019-08-24 14:18:13 +0900

    Summary
    非同期通信と関連するファイル修正

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,14 +1,42 @@
 $(function() {
-  $(function() {
-  $(".chat-group-form__input").on("keyup", function() {
+  var search_list = $("#user-search-result");
+  
+  function appendUser(user){
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+                </div>`
+    search_list.append(html);
+  }
+ 
+  function appendNewUser(id, name){
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-${id} data-id=${id} '>
+                  <input name='group[user_ids][]' type='hidden' value='${id}'>
+                  <p class='chat-group-user__name'>${name}</p>
+                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>`
+    return html;
+  }
+ 
+$("#user-search-field").on("keyup", function() {
+    $("#user-search-result").empty();
     var input = $("#user-search-field").val();
     $.ajax({
-      type: 'GET',
-      url: '/users',
+      type: "GET",
+      url: "/users",
       data: { keyword: input },
-      dataType: 'json'
+      dataType: "json"
     })
-    
-  });
-  });
+    .done(function(users){
+      if(users.length !== 0){
+        users.forEach(function(user){
+          appendUser(user);
+        });
+      }
+    })
+    .fail(function(){
+      alert("ユーザー検索に失敗しました");
+    })
+  })
+
 });
