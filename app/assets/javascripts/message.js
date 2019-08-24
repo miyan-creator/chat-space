bda62bc03dce0f09b694245d6d05dfd4889fc205
Shinichiro Miyazono <shinichiro.miyazono@gmail.com>
2019-08-24 16:29:58 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -45,4 +45,24 @@ $(function(){
     $(".form__submit").removeAttr("disabled");
   });
  })
-})
+
+ var reloadMessages = function() {
+  //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+  last_message_id = $("#data")
+  $.ajax({
+    //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+    url: '/groups/${group_id}/api/messages',
+    //ルーティングで設定した通りhttpメソッドをgetに指定
+    type: 'get',
+    dataType: 'json',
+    //dataオプションでリクエストに値を含める
+    data: {id: last_message_id}
+  })
+  .done(function(messages) {
+    console.log('success');
+  })
+  .fail(function() {
+    console.log('error');
+  });
+};
+});
